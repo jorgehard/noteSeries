e360810537df6ee0fc5148b26a82e46260f1134d
Jorge Henrique <jorgehenrique@live.com>
2020-02-20 21:40:35 -0300

    Update config.php

--- config.php
+++ config.php
@@ -2,17 +2,22 @@
 header('Last-Modified: ' . gmdate("D, d M Y H:i:s") . ' GMT');
 header('Cache-Control: no-store, no-cache, must-revalidate');
 header('Cache-Control: post-check=0, pre-check=0', false);
-header('Pragma: no-cache');
+header('Pragma: no-cache');
+
 extract($_POST); 
-extract($_GET);
-date_default_timezone_set('America/Sao_Paulo');
+extract($_GET);
+
+date_default_timezone_set('America/Sao_Paulo');
+
 class DBConnection extends PDO
 {
     public function __construct()
     {
 		try{
-			$DBhost = "localhost";			$DBuser = "root";			$DBpass = "";
-			$DBname = "noteseries";
+			$DBhost = "localhost";
+			$DBuser = "root";
+			$DBpass = "";
+			$DBname = "noteseries1";
 			parent::__construct("mysql:host=".$DBhost.";dbname=".$DBname."",$DBuser, $DBpass);
 			$this->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 		}
